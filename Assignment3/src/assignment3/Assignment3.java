a4e56343be8ef15075d9d6814d3471721b0ec7da
info <info@allsafe>
2019-12-04 16:37:08 +0100

    - u klasi Person kreirana polja i konstruktor - u glavnoj klasi splitov-an string i napravljena for petlja

--- Assignment3/src/assignment3/Assignment3.java
+++ Assignment3/src/assignment3/Assignment3.java
@@ -7,7 +7,16 @@ public class Assignment3 {
 
     public static void main(String[] args) {
         
+        String text = "John.Davidson/05082004/Belgrade Michael.Barton/01011998/Krakov Ivan.Perkinson/23051986/Moscow";
         
+        //postavljamo uslov podjele stringova
+        String uslov = "[./ ]+";
+        //kreiramo niz stringova sa uslovom
+        String[] textNiz = text.split(uslov);
+        
+        for (int i = 0; i < textNiz.length; i++) {
+            System.out.println(textNiz[i]);
+        }
     }
     
 }
--- Assignment3/src/assignment3/Person.java
+++ Assignment3/src/assignment3/Person.java
@@ -4,4 +4,17 @@ package assignment3;
 
 public class Person {
     
+    private String ime;
+    private String prezime;
+    private String datum_rodjenja;
+    private String mjesto_rodjenja;
+
+    public Person(String ime, String prezime, String datum_rodjenja, String mjesto_rodjenja) {
+        this.ime = ime;
+        this.prezime = prezime;
+        this.datum_rodjenja = datum_rodjenja;
+        this.mjesto_rodjenja = mjesto_rodjenja;
+    }
+    
+     
 }
